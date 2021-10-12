442c714f14dd9d142245d08d69c197cada87e263
Jainish-twoiq <jainish.shah@twoiq.com>
2021-10-12 15:32:16 +0530

    expansiontable

---
+++ lib/expansiontable.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ExpansionTemp extends StatefulWidget {
+  const ExpansionTemp({Key? key}) : super(key: key);
+
+  @override
+  _ExpansionTempState createState() => _ExpansionTempState();
+}
+class MyItem
+
+class _ExpansionTempState extends State<ExpansionTemp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
